b44c4b4921ac81c7f7da0071108b32d71b9d07f9
zhao-lei <lei1824964900@foxmail.com>
2016-05-24 21:39:49 -0700

    空工程

---
+++ MyYiBaiLive/MyYiBaiLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyYiBaiLive
+//
+//  Created by mac on 16/5/24.
+//  Copyright © 2016年 zhiYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
